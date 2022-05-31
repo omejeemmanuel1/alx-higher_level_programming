d81ed710f4424efac57e1eead345fc929fb49737
omejeemmanuel1 <omejeemmanuel046@yahoo.com>
2022-05-31 13:33:34 +0000

    Python magic

--- 0x00-python-hello_world/102-magic_calculation.py
+++ 0x00-python-hello_world/102-magic_calculation.py
@@ -3,10 +3,10 @@
 def magic_calculation(a, b):
         """
         Perform the computation as the following bytecode:
-         0 LOAD_CONST               1 (98)
-         3 LOAD_FAST                0 (a)
-         6 LOAD_FAST                1 (b)
-         9 BINARY_POWER
+        0 LOAD_CONST               1 (98)
+        3 LOAD_FAST                0 (a)
+        6 LOAD_FAST                1 (b)
+        9 BINARY_POWER
         10 BINARY_ADD
         11 RETURN_VALUE
         """
