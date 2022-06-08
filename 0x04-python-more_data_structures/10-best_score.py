7e2838f879d4a5be892407b4c6db964dad257afb
omejeemmanuel1 <omejeemmanuel046@yahoo.com>
2022-06-08 18:01:57 +0000

    Function that returns a key with the biggest integer value

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 
 
-def multiply_by_2(a_dictionary):
-    """ Create a new dictionary with all values doubled
+def best_score(a_dictionary):
+    """ Retrieve the key of the largest integer value
     """
     if a_dictionary is not None:
-        return {key: 2 * val for key, val in a_dictionary.items()}
+        return max(
+                a_dictionary.items(), key=lambda item: item[1], default=[None]
+            )[0]
     return None
